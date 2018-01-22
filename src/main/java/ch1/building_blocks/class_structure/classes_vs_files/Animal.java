f6894b94e8fafbc94b92e271404bc1c15795aa23
Franco Iturrizaga <fiturrizagac@gmail.com>
2018-01-22 08:58:50 -0500

    Adding first example :p

---
+++ src/main/java/ch1/building_blocks/class_structure/classes_vs_files/Animal.java
@@ -0,0 +1,15 @@
+package ch1.building_blocks.class_structure.classes_vs_files;
+
+/**
+ * There is only one public class allowed and must match with de java filename
+ * ie. Animal.java
+ */
+public class Animal {
+
+    private String name;
+
+}
+
+class Animal2 {
+
+}
---
+++ src/main/java/ch1/building_blocks/class_structure/classes_vs_files/DefaultAnimal.java
@@ -0,0 +1,5 @@
+package ch1.building_blocks.class_structure.classes_vs_files;
+
+
+class DefaultAnimal {
+}
