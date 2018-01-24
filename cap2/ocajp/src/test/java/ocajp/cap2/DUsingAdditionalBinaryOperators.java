17c9630fb623ef5406102a0c157a809f702d062a
cristian <arroyo.curi@gmail.com>
2018-01-24 00:57:31 -0500

    excercises cap2

---
+++ cap2/ocajp/src/test/java/ocajp/cap2/DUsingAdditionalBinaryOperators.java
@@ -0,0 +1,33 @@
+package ocajp.cap2;
+
+import org.junit.Test;
+
+public class DUsingAdditionalBinaryOperators {
+	
+	@Test
+	public void assignmentOperators(){
+		
+	}
+		
+	@Test
+	public void compoundAssignmentOperators(){
+		
+	}
+	
+	@Test
+	public void relationalOperators(){
+		
+	}
+	
+	@Test
+	public void logicalOperators(){
+		
+	}
+	
+	@Test
+	public void equalityOperators(){
+		
+	}
+	
+
+}
