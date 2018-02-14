442e421b31870171001a292522b6f24c3fef7627
cjarroyo <delapa123>
2018-02-13 23:29:31 -0500

    stringbuilder and equaulity

--- src/test/java/ch3/ch3/core_apis/B_Using_the_StringBuilder_Class/D_StringBuilder_vs_StringBuffer.java
+++ src/test/java/ch3/ch3/core_apis/B_Using_the_StringBuilder_Class/D_StringBuilder_vs_StringBuffer.java
@@ -6,4 +6,7 @@ package ch3.ch3.core_apis.B_Using_the_StringBuilder_Class;
  */
 
 public class D_StringBuilder_vs_StringBuffer {
+    // When writing new code that concatenates a lot of String objects together, you should use StringBuilder.
+    // StringBuilder was added to Java in Java 5. If you come across older code, you will see StringBuffer used for this purpose
+    // StringBuffer does the same thing but more slowly because it is thread safe ( You’ll learn about threads for the OCP exam).
 }
--- src/test/java/ch3/ch3/core_apis/C_Understanding_Equality/Understanding_Equality.java
+++ src/test/java/ch3/ch3/core_apis/C_Understanding_Equality/Understanding_Equality.java
@@ -5,6 +5,73 @@ package ch3.ch3.core_apis.C_Understanding_Equality;
  * on 07 February 2018 - 4:39 PM
  */
 
+import org.junit.Test;
+
 public class Understanding_Equality {
 
+    @Test
+    public void understanding_Equality_1() {
+        //you learned how to use == to compare numbers and that object references refer to the same object
+        StringBuilder one = new StringBuilder();
+        StringBuilder two = new StringBuilder();
+        StringBuilder three = one.append("a"); //chaining
+        System.out.println(one == two); // false
+        System.out.println(one == three); // true
+    }
+
+    @Test
+    public void understanding_Equality_2() {
+        //String equality, made so in part because of the way the JVM reuses String literals:
+        String x = "Hello World";
+        String y = "Hello World";
+        System.out.println(x == y); // true
+        /*Remember that Strings are immutable and literals are pooled. The JVM created only one literal in memory.
+          x and y both point to the same location in memory; therefore, the statement outputs true.*/
+    }
+
+    @Test
+    public void understanding_Equality_3() {
+        String x = "Hello World";
+        String z = " Hello World".trim();
+        System.out.println(x == z); // false
+        //Since it isn’t the same at compile-time, a new String object is created.
+    }
+
+    @Test
+    public void understanding_Equality_4() {
+        String x = new String("Hello World");
+        String y = "Hello World";
+        System.out.println(x == y); // false
+        //Since you have specifi cally requested a different String object, the pooled value isn’t shared
+    }
+
+    /*The lesson is to never use == to compare String objects. The only time you should have to deal with == for Strings is on the exam*/
+
+    @Test
+    public void understanding_Equality_5() {
+        /*You saw earlier that you can say you want logical equality rather than object equality for String objects:*/
+        String x = "Hello World";
+        String z = " Hello World".trim();
+        System.out.println(x.equals(z)); // true
+        //check the values inside the String rather than the String itself
+
+        //If you call equals() on two StringBuilder instances, it will check reference equality.
+    }
+
+    @Test
+    public void understanding_Equality_6() {
+
+        Tiger t1 = new Tiger();
+        Tiger t2 = new Tiger();
+        Tiger t3 = t1;
+        System.out.println(t1 == t1); // true
+        System.out.println(t1 == t2); // false
+        System.out.println(t1.equals(t2)); // false
+        //true: we are comparing references to the same object.
+        //false: the two object references are different
+    }
+
+    class Tiger {
+        String name;
+    }
 }
