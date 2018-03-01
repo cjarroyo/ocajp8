15931ea6c3a5cb19bb4a1697118620f21b12577f
cristian <carroyo@belatrixsf.com>
2018-03-01 18:45:40 -0500

    cap 4 private default access modifier

--- src/test/java/ch4/ch4/emthodsAndEncapsulation/C_Applying_Access_Modifiers/D_Public_Access.java
+++ src/test/java/ch4/ch4/emthodsAndEncapsulation/C_Applying_Access_Modifiers/D_Public_Access.java
@@ -6,4 +6,42 @@ package ch4.ch4.emthodsAndEncapsulation.C_Applying_Access_Modifiers;
  */
 
 public class D_Public_Access {
+
+    /*
+    TABLE 4.2 Access modifiers
+    ==========================
+
+    Can access          If that member is private?      If that member has default (package private) access?         If that member is protected?       If that member is public?
+    ___________________________________________________________________________________________________________________________________________________________________________
+
+    Member in the same class    Yes                                 Yes                                                 Yes                                     Yes
+
+    Member in another
+    class in same package       No                                  Yes                                                 Yes                                     Yes
+
+    Member in a
+    superclass in a
+    different package           No                                  No                                                  Yes                                     Yes
+
+    Method/field in a non-
+    superclass class in a
+    different package           No                                  No                                                  No                                      Yes
+    ___________________________________________________________________________________________________________________________________________________________________________
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    */
 }
