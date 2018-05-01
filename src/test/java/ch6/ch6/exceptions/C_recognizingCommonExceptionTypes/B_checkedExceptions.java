706640fbc93f9f2c6f2751256bb3936a3d2f41a7
cjarroyo <delapa123>
2018-05-01 06:39:41 -0500

    more exceptions

--- src/test/java/ch6/ch6/exceptions/C_recognizingCommonExceptionTypes/B_checkedExceptions.java
+++ src/test/java/ch6/ch6/exceptions/C_recognizingCommonExceptionTypes/B_checkedExceptions.java
@@ -6,4 +6,12 @@ package ch6.ch6.exceptions.C_recognizingCommonExceptionTypes;
  */
 
 public class B_checkedExceptions {
+    /*
+    * Checked exceptions have Exception in their hierarchy but not RuntimeException
+    * They must be handled or declared.
+    * They can be thrown by the programmer or by the JVM. Example
+    *
+    * - FileNotFoundException, Thrown programmatically when code tries to reference a file that does not exist
+    * - IOException, Thrown programmatically when there’s a problem reading or writing a file
+    * */
 }
