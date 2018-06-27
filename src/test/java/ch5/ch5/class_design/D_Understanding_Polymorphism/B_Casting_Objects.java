a5ba25aabb4513c4bc252b9b8b499163abbf9bc5
cristian <carroyo@belatrixsf.com>
2018-06-27 12:09:46 -0500

    cap 5

--- src/test/java/ch5/ch5/class_design/D_Understanding_Polymorphism/B_Casting_Objects.java
+++ src/test/java/ch5/ch5/class_design/D_Understanding_Polymorphism/B_Casting_Objects.java
@@ -7,17 +7,33 @@ package ch5.ch5.class_design.D_Understanding_Polymorphism;
 
 public class B_Casting_Objects {
 /*
-In the previous example, we created a single instance of a Lemur object and accessed it via superclass and interface references. Once we changed the reference type, though,
-we lost access to more specific methods defined in the subclass that still exist within the object. We can reclaim those references by casting the object back to the specifi c sub-
-class it came from:
+In the previous example, we created a single instance of a Lemur object and accessed it via superclass and interface references.
+Once we changed the reference type, though, we lost access to more specific methods defined in the subclass that still exist within the object.
+We can reclaim those references by casting the object back to the specific sub-class it came from:
+*/
+}
 
-    Primate primate = lemur;
-    Lemur lemur2 = primate; // DOES NOT COMPILE
-    Lemur lemur3 = (Lemur)primate;
-    System.out.println(lemur3.age);
+class Lemur3 extends Primate implements HasTail {
+    public boolean isTailStriped() {
+        return false;
+    }
 
-In this example, we first try to convert the primate reference back to a lemur reference, lemur2, without an explicit cast. The result is that the code will not compile. In the second
-example, though, we explicitly cast the object to a subclass of the object Primate and we gain access to all the methods available to the Lemur class.
+    public int age = 10;
+
+    public static void main(String[] args) {
+        Lemur lemur = new Lemur();
+
+        Primate primate = lemur;
+        //Lemur lemur2 = primate; // DOES NOT COMPILE
+        Lemur lemur3 = (Lemur) primate;
+        System.out.println(lemur3.age);
+    }
+}
+
+/*
+In this example, we first try to convert the primate reference back to a lemur reference, lemur2, without an explicit cast.
+The result is that the code will not compile. In the second example, though,
+we explicitly cast the object to a subclass of the object Primate and we gain access to all the methods available to the Lemur class.
 Here are some basic rules to keep in mind when casting variables:
 
     1. Casting an object from a subclass to a superclass doesn’t require an explicit cast.
@@ -25,8 +41,8 @@ Here are some basic rules to keep in mind when casting variables:
     3. The compiler will not allow casts to unrelated types.
     4. Even when the code compiles without issue, an exception may be thrown at runtime if the object being cast is not actually an instance of that class.
 
-The third rule is important; the exam may try to trick you with a cast that the compiler doesn’t allow. For example, we were able to cast a Primate reference to a Lemur reference,
-because Lemur is a subclass of Primate and therefore related.
+The third rule is important; the exam may try to trick you with a cast that the compiler doesn’t allow.
+For example, we were able to cast a Primate reference to a Lemur reference, because Lemur is a subclass of Primate and therefore related.
 
 Consider this example:
     public class Bird {}
@@ -37,8 +53,10 @@ Consider this example:
             Bird bird = (Bird)fish; // DOES NOT COMPILE
         }
     }
+
 In this example, the classes Fish and Bird are not related through any class hierarchy that the compiler is aware of; therefore, the code will not compile.
-Casting is not without its limitations. Even though two classes share a related hierarchy, that doesn’t mean an instance of one can automatically be cast to another. Here’s an
+Casting is not without its limitations.
+Even though two classes share a related hierarchy, that doesn’t mean an instance of one can automatically be cast to another. Here’s an
 example:
 
     public class Rodent {
@@ -51,20 +69,20 @@ example:
         }
     }
 
-This code creates an instance of Rodent and then tries to cast it to a subclass of Rodent, Capybara. Although this code will compile without issue, it will throw a
-ClassCastException at runtime since the object being referenced is not an instance of the Capybara class. The thing to keep in mind in this example is the object that was created is
-not related to the Capybara class in any way.
+This code creates an instance of Rodent and then tries to cast it to a subclass of Rodent, Capybara.
+Although this code will compile without issue, it will throw a ClassCastException at runtime since the object being referenced is not an instance of the Capybara class.
+The thing to keep in mind in this example is the object that was created is not related to the Capybara class in any way.
 ======================================================================
 Although this topic is out of scope for the OCA exam, keep in mind that the instanceof operator can be used to check whether an object belongs to a
-particular class and to prevent ClassCastExceptions at runtime. Unlike the previous example, the following code snippet doesn’t throw an exception at
-runtime and performs the cast only if the instanceof operator returns true.
+particular class and to prevent ClassCastExceptions at runtime.
+Unlike the previous example, the following code snippet doesn’t throw an exception at runtime and performs the cast only if the instanceof operator returns true.
 
     if(rodent instanceof Capybara) {
         Capybara capybara = (Capybara)rodent;
     }
 ======================================================================
 
-When reviewing a question on the exam that involves casting and polymorphism, be sure to remember what the instance of the object actually is. Then, focus on whether the
-compiler will allow the object to be referenced with or without explicit casts.
+When reviewing a question on the exam that involves casting and polymorphism, be sure to remember what the instance of the object actually is.
+Then, focus on whether the compiler will allow the object to be referenced with or without explicit casts.
  */
-}
+
