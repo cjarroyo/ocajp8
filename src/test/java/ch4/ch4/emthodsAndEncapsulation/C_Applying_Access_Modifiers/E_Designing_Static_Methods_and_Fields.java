04de049adbe935c3bd3fa7b5c2187925fbb2956a
cjarroyo <delapa123>
2018-10-24 23:38:11 -0500

    cap 4

--- src/test/java/ch4/ch4/emthodsAndEncapsulation/C_Applying_Access_Modifiers/E_Designing_Static_Methods_and_Fields.java
+++ src/test/java/ch4/ch4/emthodsAndEncapsulation/C_Applying_Access_Modifiers/E_Designing_Static_Methods_and_Fields.java
@@ -11,30 +11,24 @@ public class E_Designing_Static_Methods_and_Fields {
     They are shared among all users of the class.
     You can think of statics as being a member of the single class object that exist independently of any instances of that class
 
-    Does Each Class Have Its Own Copy of the Code? *
+    Does Each Class Have Its Own Copy of the Code?
     Each class has a copy of the instance variables. There is only one copy of the code for the instance methods.
     Each instance of the class can call it as many times as it would like.
     However, each call of an instance method (or any method) gets space on the stack for method parameters and local variables.
 
     The same thing happens for static methods. There is one copy of the code. Parameters and local variables go on the stack.
-
     Just remember that only data gets its “own copy.” There is no need to duplicate copies of the code itself.
-
     That means you can call it by the classname.
+    The main() method is a static method. That means you can call it by the classname.
      */
 }
 
-//The main() method is a static method.
-//That means you can call it by the classname.
-
 class Koala {
-
     public static int count = 0; // static variable
 
     public static void main(String[] args) { // static method
         System.out.println(count);
     }
-
 }
 /*
 We said that the JVM basically calls Koala.main() to get the program started.
@@ -45,12 +39,11 @@ class KoalaTester {
         Koala.main(new String[0]); // call static method
     }
 }
-
 /*
 static methods have two main purposes:
 
-- For utility or helper methods that don’t require any object state. Since there is no need to access instance variables,
- having static methods eliminates the need for the caller to instantiate the object just to call the method.
+- For utility or helper methods that don’t require any object state.
+ Since there is no need to access instance variables, having static methods eliminates the need for the caller to instantiate the object just to call the method.
 
 -  For state that is shared by all instances of a class, like a counter. All instances must share the same state.
  Methods that merely use that state should be static as well.
--- src/test/java/ch4/ch4/emthodsAndEncapsulation/C_Applying_Access_Modifiers/F_Calling_a_Static_Variable_or_Method.java
+++ src/test/java/ch4/ch4/emthodsAndEncapsulation/C_Applying_Access_Modifiers/F_Calling_a_Static_Variable_or_Method.java
@@ -8,7 +8,6 @@ package ch4.ch4.emthodsAndEncapsulation.C_Applying_Access_Modifiers;
 import org.junit.Test;
 
 public class F_Calling_a_Static_Variable_or_Method {
-
     //Usually, accessing a static member is easy. You just put the classname before the method or variable and you are done. Example:
     @Test
     public void calling_a_Static_Variable_or_Method_1() {
@@ -29,15 +28,13 @@ public class F_Calling_a_Static_Variable_or_Method {
         k = null;
         System.out.println(k.count); // k is still a Koala
     }
-
     /*
     Believe it or not, this code outputs 0 twice.
     Line 6 sees that k is a Koala and count is a static variable, so it reads that static variable.
     Line 8 does the same thing. Java doesn’t care that k happens to be null. Since we are looking for a static, it doesn’t matter.
-     */
+    */
 
     //what does the following output?
-
     @Test
     public void calling_a_Static_Variable_or_Method_3() {
         Koala.count = 4;
@@ -51,6 +48,5 @@ public class F_Calling_a_Static_Variable_or_Method {
     There is only one count variable since it is static.
     It is set to 4, then 6, and finally winds up as 5.
     All the Koala variables are just distractions.
-
      */
 }
